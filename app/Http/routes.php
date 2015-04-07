b03c71d772bb0bb00df81edf8c2d675e26a54c6d
lu6fer <lu6fer2604@gmail.com>
2015-04-07 20:36:50 +0200

    add: index view and controller add: bootstrap, angularjs, angularstrap cdn upd: change route to add api prefix for api and create index route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,34 +10,37 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::group(['prefix' => 'api'], function(){
+    Route::get('/user',                    'UserController@index');
+    Route::get('/user/{slug}',             'UserController@show');
+    Route::get('/user/{slug}/level',       'UserController@levels');
+    Route::get('/user/{slug}/adhesion',    'UserController@adhesion');
+    Route::get('/user/{slug}/certificate', 'UserController@certificate');
+    Route::get('/user/{slug}/article',     'UserController@articles');
+    Route::get('/user/{slug}/comment',     'UserController@comments');
+    Route::get('/user/{slug}/dive',        'UserController@diver');
+    Route::get('/user/{slug}/dive/owner',  'UserController@diveOwner');
 
+    Route::get('/address',                 'AddressController@index');
+    Route::get('/address/{id}',            'AddressController@show');
 
-Route::get('/user',                    'UserController@index');
-Route::get('/user/{slug}',             'UserController@show');
-Route::get('/user/{slug}/level',       'UserController@levels');
-Route::get('/user/{slug}/adhesion',    'UserController@adhesion');
-Route::get('/user/{slug}/certificate', 'UserController@certificate');
-Route::get('/user/{slug}/article',     'UserController@articles');
-Route::get('/user/{slug}/comment',     'UserController@comments');
-Route::get('/user/{slug}/dive',        'UserController@diver');
-Route::get('/user/{slug}/dive/owner',  'UserController@diveOwner');
+    Route::get('/article',                 'ArticleController@index');
+    Route::get('/article/{slug}',          'ArticleController@show');
+    Route::get('/article/{slug}/comment',  'ArticleController@comments');
+    Route::get('/article/{slug}/user',     'ArticleController@user');
 
-Route::get('/address',                 'AddressController@index');
-Route::get('/address/{id}',            'AddressController@show');
+    Route::get('/level',                   'LevelsController@index');
+    Route::get('/level/boat',              'LevelsController@boat');
+    Route::get('/level/dive',              'LevelsController@dive');
+    Route::get('/level/nitrox',            'LevelsController@nitrox');
+    Route::get('/level/monitor',           'LevelsController@monitor');
 
-Route::get('/article',                 'ArticleController@index');
-Route::get('/article/{slug}',          'ArticleController@show');
-Route::get('/article/{slug}/comment',  'ArticleController@comments');
-Route::get('/article/{slug}/user',     'ArticleController@user');
+    Route::get('/dive',                    'DivesController@index');
+    Route::get('/dive/{slug}/user',        'DivesController@registered');
 
-Route::get('/level',                   'LevelsController@index');
-Route::get('/level/boat',              'LevelsController@boat');
-Route::get('/level/dive',              'LevelsController@dive');
-Route::get('/level/nitrox',            'LevelsController@nitrox');
-Route::get('/level/monitor',           'LevelsController@monitor');
+    Route::get('/adhesion/insurance',      'AdhesionController@insurance');
+    Route::get('/adhesion/origin',         'AdhesionController@origin');
+});
 
-Route::get('/dive',                    'DivesController@index');
-Route::get('/dive/{slug}/user',        'DivesController@registered');
+Route::get('/',                            'HomeController@index');
 
-Route::get('/adhesion/insurance',      'AdhesionController@insurance');
-Route::get('/adhesion/origin',         'AdhesionController@origin');
